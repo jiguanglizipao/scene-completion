9c503b9f4d84a42e0a0ef4839eaef5e5806c3b9d
David Feng <jiguanglizipao@163.com>
2018-01-07 21:07:33 +0800

    add mpi gd

--- gd.cpp
+++ gd.cpp
@@ -6,6 +6,7 @@
 #include <sys/time.h>
 #include <opencv2/opencv.hpp>
 #include <omp.h>
+#include <mpi.h>
 using namespace cv;
 using namespace std;
 
@@ -63,21 +64,48 @@ int main(int argc, char** argv)
     for(string line; getline(fi, line);) files.push_back(line);
     fi.close();
     vector<vector<double>> datas(files.size());
+
+    MPI::Init();
+    size_t nproc = MPI::COMM_WORLD.Get_size();
+    size_t myid = MPI::COMM_WORLD.Get_rank();
+    size_t mylen = (files.size()+nproc-1)/nproc;
+
     #pragma omp parallel for
-    for(size_t i=0;i<files.size();i++)
+    for(size_t i=myid*mylen;i<min((myid+1)*mylen, files.size());i++)
     {
-        if(!omp_get_thread_num()) printf("%.2lf%%\n", 100.*i/files.size());
+        if(!omp_get_thread_num()) printf("myid=%lu\t%.2lf%%\n", myid, 100.*(i-myid*mylen)/(min((myid+1)*mylen, files.size())-myid*mylen));
         Mat image = imread(files[i], cv::IMREAD_COLOR);
         datas[i] = gist(image);
+        for(size_t i=myid*mylen;i<min((myid+1)*mylen, files.size());i++)
+        {
+            MPI::COMM_WORLD.Send(datas[i].data(), datas[i].size(), MPI::DOUBLE, 0, i);
+        }
     }
-    printf("100.00%%\n");
-    FILE *fo = fopen(argv[2], "w");
-    for(size_t i=0;i<files.size();i++)
+    printf("myid=%lu\t100.00%%\n", myid);
+    if(myid == 0)
     {
-        fwrite(datas[i].data(), sizeof(double), datas[i].size(), fo);
+        FILE *fo = fopen(argv[2], "w");
+        for(size_t id=0;id<nproc;id++)
+            for(size_t i=id*mylen;i<min((id+1)*mylen, files.size());i++)
+            {
+                if(id != 0)
+                {
+                    datas[i].resize(datas[0].size());
+                    MPI::COMM_WORLD.Recv(datas[i].data(), datas[i].size(), MPI::DOUBLE, id, i);
+                }
+                fwrite(datas[i].data(), sizeof(double), datas[i].size(), fo);
+            }
+        fclose(fo);
+        gettimeofday(&end, NULL);
+        printf("time %.6lf\n", double(end.tv_sec-start.tv_sec)+1e-6*double(end.tv_usec-start.tv_usec));
     }
-    fclose(fo);
-    gettimeofday(&end, NULL);
-    printf("time %.6lf\n", double(end.tv_sec-start.tv_sec)+1e-6*double(end.tv_usec-start.tv_usec));
+    else
+    {
+        for(size_t i=myid*mylen;i<min((myid+1)*mylen, files.size());i++)
+        {
+            MPI::COMM_WORLD.Send(datas[i].data(), datas[i].size(), MPI::DOUBLE, 0, i);
+        }
+    }
+    MPI::Finalize();
     return 0;
 }
